7f2d35ae5376afeac71f4b63a6e4e6c8edfcb415
Aternus <kiril@atern.us>
2026-01-27 00:05:40 +0200

    feat(refactor): improved readability

--- src/utilities/fs.zig
+++ src/utilities/fs.zig
@@ -2,7 +2,6 @@ const std = @import("std");
 const builtin = @import("builtin");
 const fs = std.fs;
 const mem = std.mem;
-
 const env_utils = @import("env.zig");
 
 pub fn pathExists(path: []const u8) bool {
