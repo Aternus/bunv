167671d3d8b59292646621b2fc9390bdbd209519
Aternus <kiril@atern.us>
2026-01-27 00:05:26 +0200

    feat(refactor): improved readability

--- src/utilities/fs.zig
+++ src/utilities/fs.zig
@@ -81,21 +81,21 @@ pub fn getBunvInstallDir(allocator: mem.Allocator, is_debug: bool) ![]const u8 {
     return bunv_install_dir;
 }
 
-pub fn getBunvVersionsDir(allocator: mem.Allocator, bunv_install_dir: []const u8) ![]u8 {
-    return try joinPath(allocator, &[_][]const u8{ bunv_install_dir, "versions" });
+pub fn getBunvVersionsDir(allocator: mem.Allocator, install_dir: []const u8) ![]u8 {
+    return try joinPath(allocator, &[_][]const u8{ install_dir, "versions" });
 }
 
-pub fn getBunVersionDir(allocator: mem.Allocator, bunv_install_dir: []const u8, version: []const u8) ![]u8 {
-    return try joinPath(allocator, &[_][]const u8{ bunv_install_dir, "versions", version });
+pub fn getBunVersionDir(allocator: mem.Allocator, install_dir: []const u8, version: []const u8) ![]u8 {
+    return try joinPath(allocator, &[_][]const u8{ install_dir, "versions", version });
 }
 
-pub fn getBunBinaryPath(allocator: mem.Allocator, bunv_install_dir: []const u8) ![]u8 {
+pub fn getBunBinaryPath(allocator: mem.Allocator, install_dir: []const u8) ![]u8 {
     if (builtin.os.tag == .windows) {
-        return try joinPath(allocator, &[_][]const u8{ bunv_install_dir, "bin", "bun.exe" });
+        return try joinPath(allocator, &[_][]const u8{ install_dir, "bin", "bun.exe" });
     }
-    return try joinPath(allocator, &[_][]const u8{ bunv_install_dir, "bin", "bun" });
+    return try joinPath(allocator, &[_][]const u8{ install_dir, "bin", "bun" });
 }
 
-pub fn getBunGlobalPackagesDir(allocator: mem.Allocator, bunv_install_dir: []const u8, version: []const u8) ![]u8 {
-    return try joinPath(allocator, &[_][]const u8{ bunv_install_dir, "versions", version, "install", "global" });
+pub fn getBunGlobalPackagesDir(allocator: mem.Allocator, install_dir: []const u8, version: []const u8) ![]u8 {
+    return try joinPath(allocator, &[_][]const u8{ install_dir, "versions", version, "install", "global" });
 }
