23487e069ae5542d4b713d6cc6dd3a36e22fbd68
Aternus <kiril@atern.us>
2026-01-27 00:00:24 +0200

    feat(refactor): improved readability

--- src/commands/list.zig
+++ src/commands/list.zig
@@ -23,18 +23,18 @@ fn printInstalledVersions(allocator: mem.Allocator, bunv_install_dir: []const u8
         return;
     }
     for (versions.items) |version| {
-        const bun_version_dir = try fs_utils.getBunVersionDir(allocator, bunv_install_dir, version);
-        defer allocator.free(bun_version_dir);
+        const version_dir = try fs_utils.getBunVersionDir(allocator, bunv_install_dir, version);
+        defer allocator.free(version_dir);
 
-        const bun_bin_path = try fs_utils.getBunBinaryPath(allocator, bun_version_dir);
-        defer allocator.free(bun_bin_path);
+        const bin_path = try fs_utils.getBunBinaryPath(allocator, version_dir);
+        defer allocator.free(bin_path);
 
-        const bun_global_packages_dir = try fs_utils.getBunGlobalPackagesDir(allocator, bunv_install_dir, version);
-        defer allocator.free(bun_global_packages_dir);
+        const global_packages_dir = try fs_utils.getBunGlobalPackagesDir(allocator, bunv_install_dir, version);
+        defer allocator.free(global_packages_dir);
 
         std.debug.print("  {s}{s}v{s}{s}\n", .{ c.bold, c.blue, version, c.reset });
-        std.debug.print("    {s}│ {s} Directory:        {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, bun_version_dir, c.reset });
-        std.debug.print("    {s}│ {s} Bin:              {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, bun_bin_path, c.reset });
-        std.debug.print("    {s}└─{s} Global Packages:  {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, bun_global_packages_dir, c.reset });
+        std.debug.print("    {s}│ {s} Directory:        {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, version_dir, c.reset });
+        std.debug.print("    {s}│ {s} Bin:              {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, bin_path, c.reset });
+        std.debug.print("    {s}└─{s} Global Packages:  {s}{s}{s}\n", .{ c.grey, c.reset, c.cyan, global_packages_dir, c.reset });
     }
 }
